0dddee46f01f8a23ea29a915ba909bc590f20508
daniela-cubides <daniela.cubides@imagineapps.co>
2023-11-21 21:44:57 -0500

    add fullPAge.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    new fullpage('#fullpage', {
+      // Opciones de Fullpage.js aquí
+      autoScrolling:true,
+      scrollHorizontally: true
+      // Puedes personalizar esto según tus necesidades
+    });
+  });
